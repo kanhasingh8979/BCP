837de9ef20e8c5cb46dde2cfa042cddcd003618b
kanha singh <kanha.singh2_bca24@gla.ac.in>
2025-08-21 10:39:39 +0530

    Update natural(5).py

--- natural(5).py
+++ natural(5).py
@@ -1,6 +1,7 @@
 '''5. Write a program to find the sum of all Natural numbers from 1 to N, where you have to take N as 
 input from user '''
-N = int(input("Enter a positive integer N: "))
+N = int(input("Enter a number N: "))
 sum_of_natural_numbers = sum(range(1, N + 1))
-print(f"The sum of all natural numbers from 1 to {N} is: {sum_of_natural_numbers}")
+print(f"The sum of all natural numbers is: {sum_of_natural_numbers}")
+
 
