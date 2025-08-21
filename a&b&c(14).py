15cf8c879b540d49add58ff4b9f1921ea21393aa
kanha singh <kanha.singh2_bca24@gla.ac.in>
2025-08-21 10:22:32 +0530

    Update a&b&c(14).py

--- a&b&c(14).py
+++ a&b&c(14).py
@@ -1,8 +1,8 @@
 '''14. Write a program to input three numbers(A, B & C) from the user and print the 
 minimum element among A, B & C. '''
-a = int(input("Enter first number (A): "))
-b = int(input("Enter second number (B): ")) 
-c = int(input("Enter third number (C): "))
+a = int(input("Enter first number  "))
+b = int(input("Enter second number  ")) 
+c = int(input("Enter third number  "))
 if a < b and a < c:
     print("Minimum element is:", a)
 elif b < a and b < c:
@@ -11,3 +11,4 @@ elif c < a and c < b:
     print("Minimum element is:", c)
 else:
     print("element is equal:")
+
