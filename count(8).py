7336ae099b808c86b339f300e5f0ae0448a04585
kanha singh <kanha.singh2_bca24@gla.ac.in>
2025-08-21 10:33:25 +0530

    Update count(8).py

--- count(8).py
+++ count(8).py
@@ -1,4 +1,5 @@
 '''8. Take an integer N as input and print the count of digits of that number. '''
-N = int(input("Enter a positive integer N: "))
+N = int(input("Enter a  number N: "))
 count_of_digits = len(str(N))
 print(f"The count of digits in {N} is: {count_of_digits}")
+
