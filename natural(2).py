c9f4baac6fdea3742bd1d5fefe5259748ca6da0b
kanha singh <kanha.singh2_bca24@gla.ac.in>
2025-08-21 10:38:53 +0530

    Update natural(2).py

--- natural(2).py
+++ natural(2).py
@@ -1,5 +1,6 @@
 '''2. Write a program to print all Natural numbers from N to 1, where you have to take N as input 
 from the user. '''
-N = int(input("Enter a positive integer N: "))
+N = int(input("Enter a number N: "))
 for i in range(N, 0, -1):
     print(i, end=' ')
+
