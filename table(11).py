8baf303875136162201a81be5415701e7f111f72
kanha singh <kanha.singh2_bca24@gla.ac.in>
2025-08-21 10:41:46 +0530

    Update table(11).py

--- table(11).py
+++ table(11).py
@@ -1,5 +1,6 @@
 '''11. Take a number A as input, print its multiplication table having the first 10 multiples''' 
-A = int(input("Enter a positive integer A: "))
+A = int(input("Enter a number A: "))
 for i in range(1, 11):
     print(f"{A} x {i} = {A * i}")
+
     
