e40407032ba829b46d6e35d87d8673919c2a1113
kanha singh <kanha.singh2_bca24@gla.ac.in>
2025-08-21 10:35:11 +0530

    Update input(1).py

--- input(1).py
+++ input(1).py
@@ -1,6 +1,7 @@
 '''1. Write a program that takes a positive integer N as input from the user and prints all natural numbers 
 numbers from 1 to N, with each number followed by a space. 
 Input:- N = 5 '''
-N = int(input("Enter a positive integer N: "))
+N = int(input("Enter a number N: "))
 for i in range(1, N + 1):
+
     print(i, end=' ')       
