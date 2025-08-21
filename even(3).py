d0b553cda6b7a86443204617e24b9059afda5eb5
kanha singh <kanha.singh2_bca24@gla.ac.in>
2025-08-21 10:34:19 +0530

    Update even(3).py

--- even(3).py
+++ even(3).py
@@ -1,6 +1,7 @@
 '''3. Write a program to print all even numbers from 1 to N, where you have to take N as input 
 from the user. 
 Input:- N = 10 '''
-N = int(input("Enter a positive integer N: "))
+N = int(input("Enter a number N: "))
 for i in range(2, N + 1, 2):
+
     print(i, end=' ')   
