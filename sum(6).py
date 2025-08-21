2a949a9ef958b7aa0401c307ccc9ae722b22dcb3
kanha singh <kanha.singh2_bca24@gla.ac.in>
2025-08-21 10:40:54 +0530

    Update sum(6).py

--- sum(6).py
+++ sum(6).py
@@ -1,5 +1,6 @@
 '''6. You are given an integer A, you need to find and return the sum of all the even numbers 
 between 1 and A. Even numbers are those numbers that are divisible by 2. '''
-A = int(input("Enter a positive integer A: "))
+A = int(input("Enter a number A: "))
 sum_of_even_numbers = sum(range(2, A + 1, 2))
-print(f"The sum of all even numbers from 1 to {A} is: {sum_of_even_numbers}")
+print(f"The sum of all even numbers  is: {sum_of_even_numbers}")
+
