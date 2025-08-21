b47155aa1e39eafc8fdd13c677009d6897c9fb67
kanha singh <kanha.singh2_bca24@gla.ac.in>
2025-08-21 10:40:04 +0530

    Update odd(4).py

--- odd(4).py
+++ odd(4).py
@@ -1,7 +1,8 @@
 '''4. Write a program to print all odd numbers from 1 to N, where you have to take N as input 
 from user. '''
-N = int(input("Enter a positive integer N: "))
+N = int(input("Enter a number N: "))
 for i in range(1, N + 1, 2):
     print(i, end=' ')
+
 
     
