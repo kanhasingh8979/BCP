ac2ca94dd289542e9b0aa837ad804d3d82b90a2a
kanha singh <kanha.singh2_bca24@gla.ac.in>
2025-08-21 10:36:09 +0530

    Update inputodd(7).py

--- inputodd(7).py
+++ inputodd(7).py
@@ -1,5 +1,6 @@
 '''7. Take an integer A as input. You have to print the sum of all odd numbers in the range [1, 
 A].'''
-A = int(input("Enter a positive integer A: "))
+A = int(input("Enter a number A: "))
 sum_of_odd_numbers = sum(range(1, A + 1, 2))
-print(f"The sum of all odd numbers from 1 to {A} is: {sum_of_odd_numbers}")
+print(f"The sum of all odd numbers is: {sum_of_odd_numbers}")
+
